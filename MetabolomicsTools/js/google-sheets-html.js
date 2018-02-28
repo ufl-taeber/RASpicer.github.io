c5c55614862a93704a61dd1bac48ed2863cc6157
RASpicer <rs809@cam.ac.uk>
2018-02-28 10:03:14 +0000

    Update google-sheets-html.js

--- MetabolomicsTools/js/google-sheets-html.js
+++ MetabolomicsTools/js/google-sheets-html.js
@@ -16,7 +16,7 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1a096jlzbAwTxUdvTtJB2bwfkBRdDcMKrkywS8YZf344');
-    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V label A "Software", B "Description", C "Functionality Category(-ies)", D "Functionality Sub-Category", E "Instrument Data Type", F "Instrument Data Type Sub-Category", G "Approaches", H "Computer Skills", I "Software Type", J "Interface", K "Operating System (OS)", L "Language", M "Version", N "Dependencies", O "Input Formats - Open", P "Input Formats - Proprietary", Q "Published", R "Last Updated", S "License", T "Website", U "Paper", V "PMID" ');
+    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W label A "Software", B "Description", C "Functionality Category(-ies)", D "Functionality Sub-Category", E "Instrument Data Type", F "Instrument Data Type Sub-Category", G "Approaches", H "Computer Skills", I "Software Type", J "Interface", K "Operating System (OS)", L "Language", M "Version", N "Dependencies", O "Input Formats - Open", P "Input Formats - Proprietary", Q "Published", R "Last Updated", S "License", T "Website", U "Paper", V "PMID" W "Test" ');
     query.send(handleQueryResponse);
 }
 
