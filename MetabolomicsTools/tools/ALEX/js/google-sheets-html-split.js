3ee6bb0976f58f80e9d59f4b81405c8467e16797
Rachel Spicer <rs809@cam.ac.uk>
2018-02-28 13:16:09 +0000

    Test individual Software Page

---
+++ MetabolomicsTools/tools/ALEX/js/google-sheets-html-split.js
@@ -0,0 +1,33 @@
+/*!
+ * 
+ * Google Sheets To HTML v0.9a
+ * 
+ * To use, simply replace the "tq?key=" value in the
+ * URL below with your own unique Google document ID
+ * 
+ * The Google document's sharing must be set to public
+ * 
+ */
+
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query(google.visualization.Query('https://spreadsheets.google.com/tq?key=1a096jlzbAwTxUdvTtJB2bwfkBRdDcMKrkywS8YZf344&output=html&range=A1');
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
