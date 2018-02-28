af00c76e20c033a0814b70b7d115bc24fc6540eb
RASpicer <rs809@cam.ac.uk>
2018-02-28 13:38:32 +0000

    Update google-sheets-html.js

--- MetabolomicsTools/tools/ALEX/js/google-sheets-html.js
+++ MetabolomicsTools/tools/ALEX/js/google-sheets-html.js
@@ -16,6 +16,24 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1a096jlzbAwTxUdvTtJB2bwfkBRdDcMKrkywS8YZf344&output=html');
+    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T label A "Software", B "Description", C "Functionality Category(-ies)", D "Functionality Sub-Category", E "Instrument Data Type", F "Instrument Data Type Sub-Category", G "Approaches", H "Computer Skills", I "Software Type", J "Interface", K "Operating System (OS)", L "Language", M "Version", N "Dependencies", O "Input Formats - Open", P "Input Formats - Proprietary", Q "Published", R "Last Updated", S "License", T "Website" ');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
+
     query.send(handleQueryResponse);
 }
 
